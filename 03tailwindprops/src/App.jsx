3990ae468350d9edf264f357d9342c3d16598d48
Supriya <supriyawakchaure0@gmail.com>
2023-11-22 20:43:21 +0530

    tailwind and props

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -2,13 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/Card'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-        <h1 className='bg-green-400'>Tailwind test</h1>   
+        <h1 className='bg-green-400 p-4 rounded-xl mb-2'>Tailwind test</h1>
+        
+        <Card />
+        <Card />
     </>   
   )
 }
