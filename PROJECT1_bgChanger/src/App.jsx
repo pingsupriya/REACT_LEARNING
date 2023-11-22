287c51883c7b58cad8a6245d327c88fe48bf9df0
Supriya <supriyawakchaure0@gmail.com>
2023-11-23 00:19:06 +0530

    bg-changer

--- PROJECT1_bgChanger/src/App.jsx
+++ PROJECT1_bgChanger/src/App.jsx
@@ -8,7 +8,37 @@ function App() {
     <div className='w-full h-screen duration-200'
         style={{backgroundColor: color}}>
           <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
-            test
+            <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-2xl'>
+              <button
+              onClick={()=>setColor("red")}
+              className='outline-none px-4 py-1 rounded-full text-white shadow-lg'
+              style={{backgroundColor:"red"}}>Red</button>
+
+              <button 
+              onClick={()=>setColor("green")}
+              className='outline-none px-4 py-1 rounded-full text-white shadow-lg'
+              style={{backgroundColor:"green"}}>Green</button>
+
+              <button 
+              onClick={()=>setColor("blue")}
+              className='outline-none px-4 py-1 rounded-full text-white shadow-lg'
+              style={{backgroundColor:"blue"}}>Blue</button>
+
+              <button 
+              onClick={()=>setColor("violet")}
+              className='outline-none px-4 py-1 rounded-full text-white shadow-lg'
+              style={{backgroundColor:"violet"}}>Violet</button>
+
+              <button 
+              onClick={()=>setColor("yellow")}
+              className='outline-none px-4 py-1 rounded-full text-white shadow-lg'
+              style={{backgroundColor:"yellow"}}>Yellow</button>
+
+              <button 
+              onClick={()=>setColor("orange")}
+              className='outline-none px-4 py-1 rounded-full text-white shadow-lg'
+              style={{backgroundColor:"orange"}}>Orange</button>
+            </div>
           </div>
     </div>
   )
