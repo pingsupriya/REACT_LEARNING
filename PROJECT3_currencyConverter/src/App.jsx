cf534cbd8d7696a09baccdc038f5a1bba6337bf2
Supriya <supriyawakchaure0@gmail.com>
2023-11-26 01:08:29 +0530

    currency converter main files

--- PROJECT3_currencyConverter/src/App.jsx
+++ PROJECT3_currencyConverter/src/App.jsx
@@ -1,19 +1,80 @@
 import { useState } from 'react'
-
-import './App.css'
+import {InputBox} from './components'
+import useCurrencyinfo from './hooks/useCurrencyinfo'  
 
 function App() {
-  
+  const [amount,setAmount]=useState(0)
+  const [from, setfrom] = useState("usd")
+  const [to, setto] = useState("inr")
+  const [convertedAmount, setConvertedAmount]=useState(0)
 
+  const Currencyinfo=useCurrencyinfo(from)
 
+  const options=Object.keys(Currencyinfo)
 
+  const swap = ()=>{
+    setfrom(to)
+    setto(from)
+    setConvertedAmount(amount)
+    setAmount(convertedAmount)
+  }
 
+  const convert=()=>{
+    setConvertedAmount(amount*Currencyinfo[to])
+  }
 
   return (
-    <>
-      <h1 className='text-3xl bg-orange-500'>CURRENCY APP CONVERTER</h1>
-    </>
-  )
+        <div
+            className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
+            style={{
+                backgroundImage: `url('https://thumbs.dreamstime.com/b/global-currency-technology-background-money-transfer-stock-market-concept-money-transfer-global-currency-stock-exchange-153251024.jpg')`,
+            }}
+        >
+            <div className="w-full">
+                <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
+                    <form
+                        onSubmit={(e) => {
+                            e.preventDefault();
+                           convert()
+                        }}
+                    >
+                        <div className="w-full mb-1">
+                            <InputBox
+                                label="From"
+                                amount={amount}
+                                currencyOptions={options}
+                                oncurrencyChange={(currency)=>setAmount(amount)}
+                                selectCurrency={from}
+                                onAmountChange={(amount)=>setAmount(amount)}                            
+                            />
+                        </div>
+                        <div className="relative w-full h-0.5">
+                            <button
+                                type="button"
+                                className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
+                                onClick={swap}
+                            >
+                                swap
+                            </button>
+                        </div>
+                        <div className="w-full mt-1 mb-4">
+                            <InputBox
+                                label="To"
+                                amount={convertedAmount}
+                                currencyOptions={options}
+                                oncurrencyChange={(currency)=>setto(currency)}
+                                selectCurrency={from}
+                                amountDisable
+                            />
+                        </div>
+                        <button type="submit" className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg">
+                            Convert {from.toUpperCase} to {to.toUpperCase}
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App
